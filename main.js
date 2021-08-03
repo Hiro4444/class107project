76baad4be37ab3a30cb2937668af01bba43403b1
XD Hiro <71639427+Hiro4444@users.noreply.github.com>
2021-08-03 18:07:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Webcam.set({
+    width:350,
+    length:300,
+    image_format:'png',
+    png_quality:90
+    });
+    camera=document.getElementById("camera");
+    Webcam.attach('#camera');
+    function snap() {
+      Webcam.snap(function(data_uri){
+          document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+      });  
+    }
+    console.log('ml5 version',ml5.version);
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/oCd_hMkjo/',modelLoaded);
+    function modelLoaded() {
+        console.log('Model Loaded!')
+    }
+    function speak() {
+        var synth=window.speechSynthesis;
+        speak_data_1="the first preduction is"+preduction_1;
+        speak_data_2="and the second preduction is"+preduction_2;
+        var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+        synth.speak(utterThis); 
+}
